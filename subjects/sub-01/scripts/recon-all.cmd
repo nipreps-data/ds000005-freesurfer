2b230f7ff84c4670de7ab219e07ff021a20e6718
Christopher J. Markiewicz <markiewicz@stanford.edu>
2023-12-04 21:50:11 -0500

    [DATALAD RUNCMD] recon-all -sd subjects -s sub-01 -i sour...
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "recon-all -sd subjects -s sub-01 -i sourcedata/ds000005/sub-01/anat/sub-01_T1w.nii.gz",
     "dsid": "ab39ddac-b99a-4488-8481-2178ff7b8d61",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

---
+++ subjects/sub-01/scripts/recon-all.cmd
@@ -0,0 +1,7 @@
+
+
+#---------------------------------
+# New invocation of recon-all Mon Dec  4 21:50:09 EST 2023 
+
+ mri_convert /data/nipreps-data/ds000005-freesurfer/sourcedata/ds000005/sub-01/anat/sub-01_T1w.nii.gz /data/nipreps-data/ds000005-freesurfer/subjects/sub-01/mri/orig/001.mgz 
+
